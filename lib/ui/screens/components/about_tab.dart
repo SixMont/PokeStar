06b2d89c7ef6c507f58a41daaccee70d67c750cb
Cecile03 <cecile.tessier@reseau.eseo.fr>
2025-01-06 14:49:23 +0100

    feat : add biological attribut

--- lib/ui/screens/components/about_tab.dart
+++ lib/ui/screens/components/about_tab.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:poke_star/models/pokeapi.dart';
 import 'package:poke_star/states/pokeapiv2_cubit.dart';
 import 'package:poke_star/states/pokeapiv2_state.dart';
+import '../../../models/pokeapiv2.dart';
 import '../../../models/specie.dart';
 
 class AboutTab extends StatelessWidget {
@@ -17,6 +19,7 @@ class AboutTab extends StatelessWidget {
           );
         } else if (state is PokeApiV2Loaded) {
           Specie specie = state.specie;
+          PokeApiV2 pokeApiV2 = state.pokeApiV2;
           return Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
@@ -44,6 +47,84 @@ class AboutTab extends StatelessWidget {
                     letterSpacing: 2.5,
                     color: Colors.black54,
                   ),
+                ),
+                const SizedBox(height: 40),
+                const Text(
+                  'Biological',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    const Text(
+                      'Anatomy : ',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 14,
+                        letterSpacing: 2.5,
+                        color: Colors.black26,
+                      ),
+                    ),
+                    Text(
+                      specie.shape.name,
+                      textAlign: TextAlign.left,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        letterSpacing: 2.5,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    const Text(
+                      'Height : ',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 14,
+                        letterSpacing: 2.5,
+                        color: Colors.black26,
+                      ),
+                    ),
+                    Text(
+                      "${pokeApiV2.height / 10} m",
+                      textAlign: TextAlign.left,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        letterSpacing: 2.5,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    const Text(
+                      'Weight : ',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 14,
+                        letterSpacing: 2.5,
+                        color: Colors.black26,
+                      ),
+                    ),
+                    Text(
+                      "${pokeApiV2.weight / 10} kg",
+                      textAlign: TextAlign.left,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        letterSpacing: 2.5,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
